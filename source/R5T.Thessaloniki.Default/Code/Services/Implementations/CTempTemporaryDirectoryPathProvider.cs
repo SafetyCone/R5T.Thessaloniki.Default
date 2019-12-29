c264f2adc4d9bfbefaec4b0bc669f374ad9bee89
MinexAutomation <minex.automation@gmail.com>
2019-12-29 02:02:10 -0800

    Added C:\Temp temporary directory path provider service implementation.

---
+++ source/R5T.Thessaloniki.Default/Code/Services/Implementations/CTempTemporaryDirectoryPathProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace R5T.Thessaloniki.Default
+{
+    /// <summary>
+    /// Provides the <see cref="CTempTemporaryDirectoryPathProvider.DirectoryPath"/> value as the temporary directory path.
+    /// </summary>
+    public class CTempTemporaryDirectoryPathProvider : ITemporaryDirectoryPathProvider
+    {
+        public const string DirectoryPath = @"C:\Temp";
+
+
+        public string GetTemporaryDirectoryPath()
+        {
+            return CTempTemporaryDirectoryPathProvider.DirectoryPath;
+        }
+    }
+}
